3d266bc3a4075d9b2cd8f8b59e4b80c19ead00e5
elnurguluzada <kuliyevelnur@gmail.com>
2020-11-03 19:54:19 +0200

    Final version of labaratory work 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         float input =  0.14913F;
 
+        float[] inputVector = new float[50];
         float[] weightsOfInput = new float[4];
         float[] weightsOfHiddenLayer = new float[4];
         float[] outputsOfHiddenLayer = new float[4];
@@ -13,54 +14,120 @@ public class Main {
         float[] updatedWeightsOfInputLayer = new float[4];
         float[] updatedWaightsOfHiddenLayer = new float[4];
         float[] biasOfHiddenLayer = new float[4];
+        float[] updatedBiasOfHiddenLayer = new float[4];
         float learningRate = 0.2F;
-        float desiredOutput ;
+        float[] desiredOutput  = new  float[50];
         float biasofOutputLayer;
         float updatedBiasofOutputLayer;
-        float receivedOutput;
+        float[] receivedOutput  = new float[50];
+        float error;
 
-        MultilayerPerceptron multilayerPerceptron = new MultilayerPerceptron();
-
+       MultilayerPerceptron multilayerPerceptron = new MultilayerPerceptron();
+       inputVector = multilayerPerceptron.initializeValuesofInputVector();
        weightsOfInput = multilayerPerceptron.initializeWeightsOfInput();
        weightsOfHiddenLayer =  multilayerPerceptron.initializeWeightsOfHiddenLayer();
        biasOfHiddenLayer = multilayerPerceptron.initializeBiasofHiddenLayer();
        biasofOutputLayer = multilayerPerceptron.initializeBiasOfOutputLayer();
-       desiredOutput =multilayerPerceptron.calculateDesiredOutput(input);
+
+       //desiredOutput =multilayerPerceptron.calculateDesiredOutput(input);
+
 
 
         System.out.println("Input = " + input);
-        System.out.println("Desired output = " + desiredOutput);
+       //System.out.println("Desired output = " + desiredOutput);
+
+
+        for(int j =0; j < 1000; j++) {
+
+
+            for (int i = 0; i < inputVector.length; i++) {
+
+                desiredOutput[i] = multilayerPerceptron.calculateDesiredOutput(inputVector[i]);
+
+                outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(inputVector[i], weightsOfInput, biasOfHiddenLayer);
+                receivedOutput[i] = multilayerPerceptron.calculateOutput(weightsOfHiddenLayer, outputsOfHiddenLayer, biasofOutputLayer);
+
+                error = desiredOutput[i] - receivedOutput[i];
+
+
+                updatedWaightsOfHiddenLayer = multilayerPerceptron.updateHiddenLayerWeights(error, learningRate);
+                updatedBiasofOutputLayer = multilayerPerceptron.updateOutputLayerBias(error, learningRate);
+                updatedLocalGradientsOfHiddenLayer = multilayerPerceptron.updateLocalGradientsOfHiddenLayer(error, updatedWaightsOfHiddenLayer, outputsOfHiddenLayer);
+                updatedBiasOfHiddenLayer = multilayerPerceptron.updateHiddenLayerBiases(updatedLocalGradientsOfHiddenLayer, learningRate);
+                updatedWeightsOfInputLayer = multilayerPerceptron.updateInputParameters(input, weightsOfInput, updatedLocalGradientsOfHiddenLayer, learningRate);
+                outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(input, updatedWeightsOfInputLayer, updatedBiasOfHiddenLayer);
+                receivedOutput[i] = multilayerPerceptron.calculateOutput(updatedWaightsOfHiddenLayer, outputsOfHiddenLayer, updatedBiasofOutputLayer);
+
+//                System.out.println("desired output = " + desiredOutput[i]);
+//                System.out.println("received output = " + receivedOutput);
+
+
+            }
+        }
+
+
+        System.out.println("*****************************************************************");
+
+
+        //  Without updating received outputs and desired outputs
+
+        for (int i = 0; i < inputVector.length; i++) {
+
+            System.out.println("Input Number  " + (i+1));
+            desiredOutput[i] = multilayerPerceptron.calculateDesiredOutput(inputVector[i]);
+            System.out.println("Desired output [" + i +"] = " + desiredOutput[i]);
+
+            outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(inputVector[i], weightsOfInput, biasOfHiddenLayer);
+            receivedOutput[i] = multilayerPerceptron.calculateOutput(weightsOfHiddenLayer, outputsOfHiddenLayer, biasofOutputLayer);
+
+            System.out.println("Recieved output[" + i +"] = " + receivedOutput[i]);
+            System.out.println("###################################################");
+        }
 
 
 
-        outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(input , weightsOfInput  , biasOfHiddenLayer);
-
-        receivedOutput = multilayerPerceptron.calculateOutput(weightsOfHiddenLayer , outputsOfHiddenLayer, biasofOutputLayer);
 
 
-        System.out.println("Received output = " + receivedOutput);
+//        outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(input , weightsOfInput  , biasOfHiddenLayer);
+//
+//        receivedOutput = multilayerPerceptron.calculateOutput(weightsOfHiddenLayer , outputsOfHiddenLayer, biasofOutputLayer);
+//
 
-       float error = desiredOutput - receivedOutput;
-        System.out.println("error (before  update) = " + error);
 
 
-       if(error != 0){
+//       // System.out.println("Received output = " + receivedOutput);
+//
+//       float error = desiredOutput - receivedOutput;
+//       // System.out.println("error (before  update) = " + error);
+//
+//
+//       if(error != 0){
+//
+//           for(int i =0; i < 100; i++){
+//
+//              updatedWaightsOfHiddenLayer = multilayerPerceptron.updateHiddenLayerWeights(error,learningRate);
+//              updatedBiasofOutputLayer = multilayerPerceptron.updateOutputLayerBias(error, learningRate);
+//              updatedLocalGradientsOfHiddenLayer = multilayerPerceptron.updateLocalGradientsOfHiddenLayer(error, updatedWaightsOfHiddenLayer, outputsOfHiddenLayer );
+//              updatedBiasOfHiddenLayer = multilayerPerceptron.updateHiddenLayerBiases(updatedLocalGradientsOfHiddenLayer , learningRate);
+//              updatedWeightsOfInputLayer = multilayerPerceptron.updateInputParameters(input , weightsOfInput, updatedLocalGradientsOfHiddenLayer,learningRate);
+//              outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(input , updatedWeightsOfInputLayer, updatedBiasOfHiddenLayer);
+//              receivedOutput = multilayerPerceptron.calculateOutput( updatedWaightsOfHiddenLayer, outputsOfHiddenLayer , updatedBiasofOutputLayer);
+//
+//               System.out.println("desired output = " + desiredOutput);
+//               System.out.println("received output = " + receivedOutput);
+//
+//              error = desiredOutput - receivedOutput;
+//            //  System.out.println("error (after update) = " + error);
+//
+//           }
+//       }
+//
+//        System.out.println("error = " + error);
+//
+//
+//
 
-           for(int i =0; i < 40; i++){
 
-              updatedWaightsOfHiddenLayer = multilayerPerceptron.updateHiddenLayerWeights(error,learningRate);
-              updatedBiasofOutputLayer = multilayerPerceptron.updateOutputLayerBias(error, learningRate);
-              updatedLocalGradientsOfHiddenLayer = multilayerPerceptron.updateLocalGradientsOfHiddenLayer(error, updatedWaightsOfHiddenLayer, outputsOfHiddenLayer );
-              updatedWeightsOfInputLayer = multilayerPerceptron.updateInputParameters(input , weightsOfInput, updatedLocalGradientsOfHiddenLayer,learningRate);
-              outputsOfHiddenLayer = multilayerPerceptron.calculateOutputsOfHiddenLayer(input , updatedWeightsOfInputLayer, biasOfHiddenLayer);
-              receivedOutput = multilayerPerceptron.calculateOutput( updatedWaightsOfHiddenLayer, outputsOfHiddenLayer , updatedBiasofOutputLayer);
-              error = desiredOutput - receivedOutput;
-              System.out.println("error (after update) = " + error);
-
-           }
-       }
-
-        System.out.println("error = " + error);
 
 
     }
--- src/com/company/MultilayerPerceptron.java
+++ src/com/company/MultilayerPerceptron.java
@@ -5,7 +5,7 @@ public class MultilayerPerceptron {
 
 
     public float input =  0.14913F;
-
+    public float[] inputVector = new float[50];
     public float[] weightsOfInputLayer = new float[4];
     public float   biasofInput;
     public float[] weightsOfHiddenLayer = new float[4];
@@ -18,6 +18,17 @@ public class MultilayerPerceptron {
 
 
 
+
+    float[] initializeValuesofInputVector(){
+
+        for (int i = 0; i<inputVector.length; i++){
+
+            inputVector[i] = (float) (Math.random() * (1 - (0)));
+
+            System.out.println("inputVector[" +i+"] = " + inputVector[i]);
+        }
+        return inputVector;
+    }
 
     float[] initializeWeightsOfInput(){
 
@@ -93,6 +104,10 @@ public class MultilayerPerceptron {
     float[] calculateOutputsOfHiddenLayer( float input ,float[] weightsOfInput , float[] biasOfHiddenLayer ){
         float error ;
         for (int i = 0; i <VofHiddenLayers.length; i++){
+//            System.out.println("**************************");
+//            System.out.println("weightsOfInput[" + i +"] = " + weightsOfInput[i]);
+//            System.out.println("biasOfHiddenLayer[" + i +"] = " + biasOfHiddenLayer[i]);
+//            System.out.println("**************************");
             VofHiddenLayers[i] = input * weightsOfInput[i] +  biasOfHiddenLayer[i];
             if(!Float.isNaN((float) Math.exp(-VofHiddenLayers[i]))){
                 outputsOfHiddenLayers[i] = (float) (1 / (1 + Math.exp(-VofHiddenLayers[i])));
@@ -112,27 +127,52 @@ public class MultilayerPerceptron {
         for (int i = 0; i < outputsOfHiddenLayers.length; i++) {
 
 
-            outputOfOutputLayer += outputsOfHiddenLayers[i] * weightsOfHiddenLayer[i] + biasOfOutputLayer;
+          //  System.out.print(" (outputsOfHiddenLayers["+i+"]) " +outputsOfHiddenLayers[i] + " * " + "(weightsOfHiddenLayer[" +i+"]) " +weightsOfHiddenLayer[i]);
+            outputOfOutputLayer  = outputsOfHiddenLayers[i] * weightsOfHiddenLayer[i] ;
+
         }
 
+      //  System.out.print(" + (biasOfOutputLayer) " + biasOfOutputLayer );
+        outputOfOutputLayer  = outputOfOutputLayer + biasOfOutputLayer;
+      //  System.out.println(" = (outputOfOutputLayer) " + outputOfOutputLayer );
+//
+//        System.out.println(" = (outputOfOutputLayer) " +  outputOfOutputLayer);
         return outputOfOutputLayer;
 
     }
 
 
     float[] updateHiddenLayerWeights(float error, float learningRate){
-
+        System.out.println();
+        System.out.println("******** Updating Hidden Layer Weights *********   ");
+        System.out.println();
+        System.out.println("weightsOfHiddenLayer[i] = weightsOfHiddenLayer[i] + learningRate * error * outputsOfHiddenLayers[i]");
         for (int i = 0; i < weightsOfHiddenLayer.length; i++){
-            weightsOfHiddenLayer[i]+= weightsOfHiddenLayer[i] + learningRate*error*outputsOfHiddenLayers[i];
+            System.out.println();
+            System.out.print(weightsOfHiddenLayer[i] + " + " + learningRate + " * " + error + " * " + outputsOfHiddenLayers[i] );
+            weightsOfHiddenLayer[i] = weightsOfHiddenLayer[i] + learningRate * error * outputsOfHiddenLayers[i];
+            System.out.println(" = (weightsOfHiddenLayer) " + weightsOfHiddenLayer);
+
+            System.out.println("Updated weightsOfHiddenLayer[" +i+ "] = " + weightsOfHiddenLayer[i]);
         }
+        System.out.println();
         return weightsOfHiddenLayer;
     }
 
 
     float updateOutputLayerBias(float error, float learningRate){
 
-        biasOfOutputLayer+= biasOfOutputLayer + learningRate*error;
+        System.out.println("******** Updating Output Layer Bias  ********* ");
+        System.out.println();
 
+        System.out.println("  biasOfOutputLayer + learningRate * error");
+        System.out.print(biasOfOutputLayer + " = " + biasOfOutputLayer + " + " + learningRate + " * " + error );
+        biasOfOutputLayer  =  biasOfOutputLayer + learningRate * error;
+        System.out.println(" = (updatedBiasOfOutputLayer) " + biasOfOutputLayer);
+
+        System.out.println("Updated biasOfOutputLayer = " + biasOfOutputLayer);
+
+        System.out.println();
       return biasOfOutputLayer;
     }
 
@@ -140,23 +180,41 @@ public class MultilayerPerceptron {
 
     float[] updateLocalGradientsOfHiddenLayer(float error , float[] updatedWeightsOfOutputLayer, float[] outputsOfHiddenLayers){
 
+        System.out.println("******** Updating Local Gradients Of HiddenLayer  ********* ");
+        System.out.println();
         for (int i = 0; i < localGradientsOfHiddenLayer.length; i++){
 
-            localGradientsOfHiddenLayer[i] += outputsOfHiddenLayers[i]*(1-outputsOfHiddenLayers[i])*error*updatedWeightsOfOutputLayer[i];
+            localGradientsOfHiddenLayer[i]  = outputsOfHiddenLayers[i]*(1-outputsOfHiddenLayers[i])*error*updatedWeightsOfOutputLayer[i];
+            System.out.println("Updated localGradientsOfHiddenLayer[" +i+ "] = " + localGradientsOfHiddenLayer[i]);
+        }
+        System.out.println();
+        return localGradientsOfHiddenLayer;
+    }
+
+
+    float[] updateHiddenLayerBiases(  float[] localGradientsOfHiddenLayer, float learningRate){
+
+        for (int i = 0; i < biasOfHiddenLayer.length; i++){
+            biasOfHiddenLayer[i] = biasOfHiddenLayer[i] + localGradientsOfHiddenLayer[i] *  learningRate;
         }
 
-        return localGradientsOfHiddenLayer;
+        return biasOfHiddenLayer;
     }
 
 
     float[] updateInputParameters(float input , float[] weightsOfInputLayer ,float[] localGradientsOfHiddenLayer, float learningRate ){
 
+        System.out.println("******** Updating Input Parameters  ********* ");
+
         for (int i = 0; i < weightsOfInputLayer.length; i++){
 
-            weightsOfInputLayer[i]+= weightsOfInputLayer[i] + learningRate * localGradientsOfHiddenLayer[i] * input;
+            System.out.println("localGradientsOfHiddenLayer[" + i +"] = " + localGradientsOfHiddenLayer[i]);
+            weightsOfInputLayer[i] = weightsOfInputLayer[i] + learningRate * localGradientsOfHiddenLayer[i] * input;
+            System.out.println("Updated weightsOfInputLayer[" +i+ "] = " + weightsOfInputLayer[i]);
 
         }
 
+        System.out.println();
         return weightsOfInputLayer;
     }
 
